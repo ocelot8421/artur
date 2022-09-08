f83f06187eea68357d45fd1634c9566a865e93a9
ocelo8421 <8ocelot@gmail.com>
2022-09-08 19:39:54 +0200

    laptop 20220908 1939

--- js/main_v3.js
+++ js/main_v3.js
@@ -12,6 +12,13 @@ const keysMedicine = {
 
 const keysEmptyRow = { id: '', medicineName: '', dose: '', unit: '', pieces: '', piecesUnit: '' }
 
+// const connectionIntake = { id: '', day_of_week_id: '', date_id: '', medicine_id: '', time_of_day_id: '' }
+// const connectionIntake = { id: 0, dayOfWeek: 0, date: 0, medicine: 0, timeOfDay: 0 }
+const connectionIntake = {
+    id: 0, dayOfWeek: 0, date: 0, timeOfDay: 0,
+    medicine: { name: '', dose: '', unit: '', pieces: '', piecesUnit: '' }
+}
+
 
 // Get data from server.
 function getServerData(url) {
@@ -50,9 +57,7 @@ function fillDataTable(data, tableID) {
 
 //Create the table for the day of week
 function createDay(dayData, divDay, tBody) {
-    // let plusButton = createPlusButton("btn btn-primary", '<i class="fa fa-solid fa-plus" aria-hidden="true"></i>');
     let refreshButton = createButton("btn btn-info", "setRow(this)", '<i class="fa fa-refresh" aria-hidden="true"></i>');
-    // let bGroupDay = [plusButton, refreshButton];
     let bGroupDay = [refreshButton];
     let tr = createAnyElement("tr", {
         class: `time`
@@ -61,8 +66,6 @@ function createDay(dayData, divDay, tBody) {
     divDay.appendChild(tr);
     fillDayRow(dayData, tr, keysDayOfWeek, divDay);
     tr.appendChild(btnGroup);
-    // let bFunction = "createEmptyIntakeRow(this.parentElement.parentElement.parentElement.parentElement, this)";
-    // plusButton.setAttribute("onclick", bFunction);
     tBody.appendChild(divDay);
 }
 
@@ -207,8 +210,8 @@ function createEmptyIntakeRow(div) {
         let tdEmpty = createAnyElement("td");
         let input = createAnyElement("input", {
             class: `form-control newRowBox`,
-            name: k,
-            value: "new " + k
+            name: k
+            // value: "new " + k
         });
         switch (k) {
             case "id":
@@ -252,22 +255,36 @@ function createButton(bClass, bFunction, bIcon) {
     return button;
 }
 
-//Collect all data for pdfEnvilope
-function getDayData(trTimeOfDay) {
-    let dataForEnvelope = {};
+//Collect all data from intake row
+// function collectIntakeData(trMedicine) { //20220907 1235 ----------------------------------
+//     console.log("collectIntakeData:");
+//     inputsElementsFromMedicineRow = trMedicine.querySelectorAll('input');
+//     let medicineObject = {};
+//     for (let i = 0; i < inputsElementsFromMedicineRow.length; i++) {
+//         medicineObject[inputsElementsFromMedicineRow[i].name] = inputsElementsFromMedicineRow[i].value;
+//     }
+//     console.log(medicineObject);
+// }
 
+
+//Collect all data for pdfEnvilope
+function getDayDataForEnevelope(trTimeOfDay) {
+    let dataForEnvelope = {};
+    //data from day of week row (1st row)
     let inputElementsFromDayOfWeekRow = trTimeOfDay.parentElement.parentElement.querySelectorAll(`.forDayDataSearching`);
     for (let i = 0; i < inputElementsFromDayOfWeekRow.length; i++) {
         if (inputElementsFromDayOfWeekRow[i].value !== "undefined") {
             dataForEnvelope[inputElementsFromDayOfWeekRow[i].name] = inputElementsFromDayOfWeekRow[i].value;
         }
     }
+    // data from time of day row (above medicines)
     let inputElementsFromTimeOfDayRow = trTimeOfDay.querySelectorAll('input');
     for (let i = 0; i < inputElementsFromTimeOfDayRow.length; i++) {
         if (inputElementsFromTimeOfDayRow[i].value !== "undefined") {
             dataForEnvelope[inputElementsFromTimeOfDayRow[i].name] = inputElementsFromTimeOfDayRow[i].value;
         }
     }
+    // data of medicines (medicineNAme, dose, unit, pieces, pieceUnit)
     dataForEnvelope["medicines"] = [];
     let arrayOfMedicineRows = trTimeOfDay.nextSibling.children;
     for (trMedicine of arrayOfMedicineRows) {
@@ -278,6 +295,7 @@ function getDayData(trTimeOfDay) {
         }
         dataForEnvelope.medicines.push(medicineObject);
     }
+    //data from day-of-month column to create the day-of-month row on week table in envilope
     let inputElementsFromDayOfMonthColumn = document.querySelectorAll(".dayOfMonth");
     for (let i = 0; i < inputElementsFromDayOfMonthColumn.length; i++) {
         dataForEnvelope[`weekDay${i}`] = inputElementsFromDayOfMonthColumn[i].value;
@@ -285,18 +303,46 @@ function getDayData(trTimeOfDay) {
     return dataForEnvelope;
 }
 
-function getMedicineIntakeID(tr) {
+//Search connection between day of week, time of day and medication intake, then generate an object for push request
+function collectConnections(tr) {
     let intakeID = 0;
-    intakeID += (parseInt(tr.parentElement.parentElement.previousElementSibling.firstChild.firstChild.value) * 1000);
-    intakeID += (parseInt(tr.parentElement.previousElementSibling.firstChild.firstChild.value) * 100);
-    intakeID += parseInt(tr.firstChild.firstChild.value);
-    return intakeID;
+    let conDayOfWeek = tr.parentElement.parentElement.previousElementSibling.firstChild.firstChild.value;
+    intakeID += (parseFloat(conDayOfWeek) * 1000);
+    let conTimeOfDay = tr.parentElement.previousElementSibling.firstChild.firstChild.value;
+    intakeID += (parseFloat(conTimeOfDay) * 100);
+    // let conMedIntake = tr.firstChild.firstChild.value;
+    // intakeID += parseFloat(conMedIntake);
+
+    console.log("collectConnections - medicine:");
+    console.log(tr);
+
+    connectionIntake.id = intakeID;
+    connectionIntake.dayOfWeek = conDayOfWeek;
+    connectionIntake.date = conDayOfWeek; // temporary. it will need modification at annual diary!!!!!
+    connectionIntake.timeOfDay = conTimeOfDay;
+    // connectionIntake.medicine = conMedIntake;
+    let medicineName = tr.querySelector('input[name="medicineName"]').value;
+    let dose = tr.querySelector('input[name="dose"]').value;
+    let unit = tr.querySelector('input[name="unit"]').value;
+    let pieces = tr.querySelector('input[name="pieces"]').value;
+    let piecesUnit = tr.querySelector('input[name="piecesUnit"]').value;
+    connectionIntake.medicine.name = medicineName;
+    connectionIntake.medicine.dose = dose;
+    connectionIntake.medicine.unit = unit;
+    connectionIntake.medicine.pieces = pieces;
+    connectionIntake.medicine.piecesUnit = piecesUnit;
+
+
+    console.log("connectionIntake:"); // put -----------------------------------
+    console.log(connectionIntake);
+
+    return connectionIntake;
 }
 
 // Delete the whole row
 function delRow(button) {
     let tr = button.parentElement.parentElement.parentElement;
-    let data = getMedicineIntakeID(tr); //HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
+    let data = collectConnections(tr);
     let fetchOptions = {
         method: "DELETE",
         mode: "cors",
@@ -304,10 +350,9 @@ function delRow(button) {
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify(data)
+        body: JSON.stringify(data.id)
     };
-    // http://localhost:8080/medicines/del/
-    fetch(`http://localhost:8080/medicines/del/${data}`, fetchOptions)
+    fetch(`http://localhost:8080/medicines/del/${data.id}`, fetchOptions)
         .then(response => response.json())
         .catch(error => console.error(error))
         .then(() => startGetMedication())
@@ -316,15 +361,17 @@ function delRow(button) {
 
 function pdfEnvilope(button) {
     let trTimeOfDay = button.parentElement.parentElement.parentElement;
-    let data = getDayData(trTimeOfDay);
+    let data = getDayDataForEnevelope(trTimeOfDay);
     let dataJson = JSON.stringify(data);
-    takeDataFromOpenSite(dataJson); //++++++++++++++++++3
+    takeDataFromOpenSite(dataJson);
 }
 
 //Set data int the whole row
 function setRow(button) {
     let tr = button.parentElement.parentElement.parentElement;
-    let data = getDayData(tr);
+    let data = collectConnections(tr);
+    console.log("data:");
+    console.log(data); // put -------------------------
     let fetchOptions = {
         method: "PUT",
         mode: "cors",
@@ -334,7 +381,7 @@ function setRow(button) {
         },
         body: JSON.stringify(data)
     };
-    fetch(`http://localhost:8080/intakes/put/${data.id}`, fetchOptions)
+    fetch(`http://localhost:8080/medicines/put/${data.id}`, fetchOptions)
         .then(response => response.json())
         .catch(error => console.error(error))
         .then(() => startGetMedication())
